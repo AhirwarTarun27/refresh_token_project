691e23278237bc9d413debedbdfa6d3524d99b38
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-05-09 00:05:59 +0530

    delete refresh token api

--- authServer.js
+++ authServer.js
@@ -5,20 +5,30 @@ const app = express();
 
 app.use(express.json());
 
-const refreshTokens = [];
+let refreshTokens = [];
 
 app.post("/token", (req, res) => {
   const refreshToken = req.body.token;
+
   if (refreshToken == null) return res.sendStatus(401);
+
   if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     const accessToken = generateAccesToken({ name: user.name });
+
     res.json({ accessToken });
   });
 });
 
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
+
 app.post("/login", (req, res) => {
+  //Authenticate User
   const username = req.body.username;
   const user = { name: username };
   const accessToken = generateAccesToken(user);
@@ -30,7 +40,7 @@ app.post("/login", (req, res) => {
 });
 
 function generateAccesToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1hr" });
 }
 
 app.listen(4000);
