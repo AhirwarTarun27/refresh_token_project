163037913eafe5c31640ee232194d49f1f56529d
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-05-08 20:15:39 +0530

    updated

--- authServer.js
+++ authServer.js
@@ -5,12 +5,26 @@ const app = express();
 
 app.use(express.json());
 
+const refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccesToken({ name: user.name });
+    res.json({ accessToken });
+  });
+});
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
   const accessToken = generateAccesToken(user);
   //refresh token
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
 
   res.json({ accessToken, refreshToken });
 });
